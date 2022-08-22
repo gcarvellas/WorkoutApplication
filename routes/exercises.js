93bdddbd7d0e9fc4b4ea2cde3d47770a8e8aecd8
gcarvellas <gcarvellas@castlepointanime.com>
2022-08-22 13:17:01 -0400

    Fixed exercisesByName route

--- routes/exercises.js
+++ routes/exercises.js
@@ -8,6 +8,111 @@ const validation = data.validation;
 const util = require("./utilities");
 
 //TODO
+
+router
+    .route('/exercise/exercisesByName')
+    .get(async (req, res) => {
+        try{
+            let searchValue = req.query.input;
+            if (typeof searchValue === 'string' && searchValue.trim() === '') {
+                return res.status(200).json({"exercises": []});
+            }
+            searchValue = validation.verifyMessage(searchValue, 'Provided value of exercise name');
+            let results = await exercises.getExercisesByName(searchValue, 5);
+            return res.status(200).json({"exercises": results});
+        }
+        catch (e) {
+            res.status(400).json({"error": e});
+        }
+    });
+
+router
+    .route('/exercises/new')
+    .get(async (req, res) => {
+        let user, password;
+
+        if(!req.session.user) {
+            return res.status(403).redirect('/signin');
+        }
+
+        try {
+            [user, password] = await util.getAuthUsernameAndPasswordFromSession(req);
+            res.status(200).render('exercises/exerciseForm', {loggedIn: (user ? true : false), muscleGroups: validation.MUSCLE_GROUPS, pageTitle: "Create Exercise", urlAfter: `/exercises/new`});
+        } catch (e) {
+            if (req.session.user) {
+                res.status(400).render('exercises/exerciseForm', {loggedIn: (user ? true: false), error: {title: 'Unable to fetch user deatils', message: e}, pageTitle: "Create Exercise"});
+            } else {
+                res.status(400).render('exercises/exerciseForm', {error: {title: 'Unable to fetch user details', message: e}, pageTitle: "Create Exercise"});
+            }
+        }
+    })
+    .post(async (req, res) => {
+        let user, password, name, note, exercise;
+        let muscles = [];
+        let equipment = [];
+
+        if(!req.session.user) {
+            return res.status(403).redirect('/signin');
+        }
+
+        try{
+            [user, password] = await util.getAuthUsernameAndPasswordFromSession(req);
+        } catch (e) {
+            if (req.session.user) {
+                res.status(400).render('exercises/exerciseForm', {loggedIn: (user ? true: false), error: {title: 'Unable to fetch user deatils', message: e}, pageTitle: "Create Exercise"});
+            } else {
+                res.status(400).render('exercises/exerciseForm', {error: {title: 'Unable to fetch user details', message: e}, pageTitle: "Create Exercise"});
+            }
+        }
+
+        try{
+            [name, muscles, note, equipment] = await util.parseExerciseForm(req);
+            exercise = await exercises.createExercise(user, password, name, muscles, equipment, note);
+
+            if (req.session.user) {
+                res.render('layouts/landingPage', {loggedIn: true, muscleGroup : validation.MUSCLE_GROUPS});
+              } else {
+                res.render('layouts/landingPage', {muscleGroup : validation.MUSCLE_GROUPS});
+              }
+        } catch (e) {
+            if (req.session.user) {
+                res.status(400).render('exercises/exerciseForm', {loggedIn: (user ? true: false), errorAfter: e, pageTitle: "Create Exercise", muscleGroups: validation.MUSCLE_GROUPS});
+            } else {
+                res.status(400).render('exercises/exerciseForm', {errorAfter: e, pageTitle: "Create Exercise", muscleGroups: validation.MUSCLE_GROUPS});
+            }
+        }
+    });
+
+router 
+    .route('/exercise/:id/edit')
+    .get(async (req, res) => {
+        let exerciseId = req.params.id;
+        let user, password;
+
+        if (!req.session.user) {
+            return res.status(403).redirect('/signin');
+        } 
+
+        try {
+            [user, password] = await util.getAuthUsernameAndPasswordFromSession(req);
+            exerciseId = validation.verifyUUID(req.params.id, 'exercise id');
+            exerciseFound = await exercises.getExercise(exerciseId);
+
+            if(user._id !== exerciseFound.user) {
+                return res.status(403).redirect('/signin');
+            }
+        } catch (e) {
+            if (req.session.user) {
+                res.status(400).render('exercises/exerciseForm', {loggedIn: true, error: {title: 'Unable to fetch exercise details', message: e}, pageTitle: "Edit Exercise"});
+            } else {
+                res.status(400).render('exercises/exerciseForm', {error: {title: 'Unable to fetch exercise details', message: e}, pageTitle: "Edit Exercise"});
+            }
+            return;
+        }
+        
+        res.status(200).render('exercises/exerciseForm', {loggedIn: (user ? true : false), exercise: exerciseFound, muscleGroups: validation.MUSCLE_GROUPS, pageTitle: "Edit Exercise",  urlAfter: `/exercise/${exerciseId}`});
+    });
+
 router
     .route('/exercise/:id')
     .get(async (req, res) => {
@@ -103,107 +208,6 @@ router
                 res.status(400).render('exercises/exerciseForm', {errorAfter: e});
             }
             return;
-        }
-    });
-
-router
-    .route('/exercise/exercisesByName')
-    .get(async (req, res) => {
-        try{
-            let searchValue = req.query.input;
-            searchValue = validation.verifyMessage(searchValue, 'Provided value of exercise name');
-            let results = await exercises.getExercisesByName(searchValue, 5);
-            return res.status(200).json({"exercises": results});
-        }
-        catch (e) {
-            res.status(400).json({"error": e});
-        }
-    });
-
-router 
-    .route('/exercise/:id/edit')
-    .get(async (req, res) => {
-        let exerciseId = req.params.id;
-        let user, password;
-
-        if (!req.session.user) {
-            return res.status(403).redirect('/signin');
-        } 
-
-        try {
-            [user, password] = await util.getAuthUsernameAndPasswordFromSession(req);
-            exerciseId = validation.verifyUUID(req.params.id, 'exercise id');
-            exerciseFound = await exercises.getExercise(exerciseId);
-
-            if(user._id !== exerciseFound.user) {
-                return res.status(403).redirect('/signin');
-            }
-        } catch (e) {
-            if (req.session.user) {
-                res.status(400).render('exercises/exerciseForm', {loggedIn: true, error: {title: 'Unable to fetch exercise details', message: e}, pageTitle: "Edit Exercise"});
-            } else {
-                res.status(400).render('exercises/exerciseForm', {error: {title: 'Unable to fetch exercise details', message: e}, pageTitle: "Edit Exercise"});
-            }
-            return;
-        }
-        
-        res.status(200).render('exercises/exerciseForm', {loggedIn: (user ? true : false), exercise: exerciseFound, muscleGroups: validation.MUSCLE_GROUPS, pageTitle: "Edit Exercise",  urlAfter: `/exercise/${exerciseId}`});
-    });
-
-router
-    .route('/exercises/new')
-    .get(async (req, res) => {
-        let user, password;
-
-        if(!req.session.user) {
-            return res.status(403).redirect('/signin');
-        }
-
-        try {
-            [user, password] = await util.getAuthUsernameAndPasswordFromSession(req);
-            res.status(200).render('exercises/exerciseForm', {loggedIn: (user ? true : false), muscleGroups: validation.MUSCLE_GROUPS, pageTitle: "Create Exercise", urlAfter: `/exercises/new`});
-        } catch (e) {
-            if (req.session.user) {
-                res.status(400).render('exercises/exerciseForm', {loggedIn: (user ? true: false), error: {title: 'Unable to fetch user deatils', message: e}, pageTitle: "Create Exercise"});
-            } else {
-                res.status(400).render('exercises/exerciseForm', {error: {title: 'Unable to fetch user details', message: e}, pageTitle: "Create Exercise"});
-            }
-        }
-    })
-    .post(async (req, res) => {
-        let user, password, name, note, exercise;
-        let muscles = [];
-        let equipment = [];
-
-        if(!req.session.user) {
-            return res.status(403).redirect('/signin');
-        }
-
-        try{
-            [user, password] = await util.getAuthUsernameAndPasswordFromSession(req);
-        } catch (e) {
-            if (req.session.user) {
-                res.status(400).render('exercises/exerciseForm', {loggedIn: (user ? true: false), error: {title: 'Unable to fetch user deatils', message: e}, pageTitle: "Create Exercise"});
-            } else {
-                res.status(400).render('exercises/exerciseForm', {error: {title: 'Unable to fetch user details', message: e}, pageTitle: "Create Exercise"});
-            }
-        }
-
-        try{
-            [name, muscles, note, equipment] = await util.parseExerciseForm(req);
-            exercise = await exercises.createExercise(user, password, name, muscles, equipment, note);
-
-            if (req.session.user) {
-                res.render('layouts/landingPage', {loggedIn: true, muscleGroup : validation.MUSCLE_GROUPS});
-              } else {
-                res.render('layouts/landingPage', {muscleGroup : validation.MUSCLE_GROUPS});
-              }
-        } catch (e) {
-            if (req.session.user) {
-                res.status(400).render('exercises/exerciseForm', {loggedIn: (user ? true: false), errorAfter: e, pageTitle: "Create Exercise", muscleGroups: validation.MUSCLE_GROUPS});
-            } else {
-                res.status(400).render('exercises/exerciseForm', {errorAfter: e, pageTitle: "Create Exercise", muscleGroups: validation.MUSCLE_GROUPS});
-            }
         }
     });
 
